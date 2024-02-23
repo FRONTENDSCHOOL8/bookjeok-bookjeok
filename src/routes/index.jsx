17cd1d17f0203c2fbac1ce9c8cfc6a89ac622224
yuna <rnssue@naver.com>
2024-02-23 23:08:24 +0900

    [chore] AtomMaking 분리

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,11 +1,4 @@
-import {
-  MainButton,
-  ModalButton,
-  OutlineButton,
-  SmallButton,
-} from '@/components/Atoms';
-import { SignUp } from '@/pages/index';
-import { Link } from 'react-router-dom';
+import AtomMaking from '@/AtomMaking';
 import Erro404 from '@/pages/Error404/Erro404';
 import { Intro, SignUp } from '@/pages/index';
 import { createBrowserRouter } from 'react-router-dom';
@@ -17,37 +10,8 @@ const routes = [
     errorElement: <Erro404 />,
   },
   {
-    path: '/atoms',
-    element: (
-      <>
-        <MainButton type="button" className="large_primary">
-          버튼
-        </MainButton>
-        <MainButton type="button" className="large_secondary">
-          버튼
-        </MainButton>
-        <MainButton disabled type="button">
-          버튼
-        </MainButton>
-        <ModalButton type="button" className="large_primary">
-          버튼
-        </ModalButton>
-        <ModalButton type="button" className="large_secondary">
-          버튼
-        </ModalButton>
-        <ModalButton disabled type="button">
-          버튼
-        </ModalButton>
-        <OutlineButton type="button">버튼</OutlineButton>
-        <OutlineButton disabled type="button">
-          버튼
-        </OutlineButton>
-        <SmallButton type="button">버튼</SmallButton>
-        <SmallButton disabled type="button">
-          버튼
-        </SmallButton>
-      </>
-    ),
+    path: '/atomMaking',
+    element: <AtomMaking />,
   },
   {
     path: '/signup',
