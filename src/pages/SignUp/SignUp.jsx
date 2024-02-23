2e9c01e77350de979ed0ab3a6972e1b2df0bf596
jio <pennicillium@daum.net>
2024-02-23 15:31:14 +0900

    [Feat] 회원가입 마크업 #9

--- src/pages/SignUp/SignUp.jsx
+++ src/pages/SignUp/SignUp.jsx
@@ -1,5 +1,62 @@
+import { Link, Form } from 'react-router-dom';
+const userInfo = {
+  basicInfo: {},
+  detailInfo: {},
+};
+
+//usedeffered value
+function BasicInfo() {
+  return (
+    <>
+      <h1>회원가입</h1>
+      <h2>기본 정보</h2>
+      <Form className="flex flex-col" action="">
+        <label htmlFor="">이메일</label>
+        <input type="email" />
+        <label htmlFor="">비밀번호</label>
+        <input type="password" />
+        <label htmlFor="">비밀번호확인</label>
+        <input type="password" />
+      </Form>
+      <Link to={{}} state={{}}>
+        다음
+      </Link>
+    </>
+  );
+}
+
+function DetailInfo() {
+  return (
+    <>
+      <h1>회원가입</h1>
+      <h2>상세 정보</h2>
+      <Form className="flex flex-col" action="">
+        <label htmlFor="">닉네임</label>
+        <input type="text" />
+        <label htmlFor="">휴대폰</label>
+        <input type="text " />
+        <label htmlFor="">생년월일</label>
+        <input type="date" />
+        <div>
+          <label htmlFor="male">남자</label>
+          <input type="radio" name="gender" id="male" />
+          <label htmlFor="female">여자</label>
+          <input type="radio" name="gender" id="female" />
+        </div>
+      </Form>
+      <Link to={{}} state={{}}>
+        다음
+      </Link>
+    </>
+  );
+}
 function SignUp() {
-  return <div>SignUp</div>;
+  return (
+    <>
+      <BasicInfo />
+      <DetailInfo />
+    </>
+  );
 }
 
 export default SignUp;
