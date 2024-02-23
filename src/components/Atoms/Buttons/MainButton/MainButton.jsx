a29c959a960644b215c65b16eef391eb8aa75c29
yuna <rnssue@naver.com>
2024-02-23 19:01:42 +0900

    [feat] 버튼 컴포넌트 추가

--- src/components/Atoms/Buttons/MainButton/MainButton.jsx
+++ src/components/Atoms/Buttons/MainButton/MainButton.jsx
@@ -1,21 +1,30 @@
 import { string, bool } from 'prop-types';
 
-function MainButton({ type, disabled, children }) {
+function MainButton({ className, type, disabled, children }) {
   const buttonStyle = {
-    className:
-      'w-[344px] h-[64px] rounded-5xl bg-bjyellow-400 text-base font-medium disabled:bg-bjgray-300 disabled:text-bjgray-500',
+    large_primary:
+      'bg-bjyellow-400 hover:bg-bjyellow-500 active:bg-bjyellow-500 focus:bg-bjyellow-500 focus:outline-none',
+    large_secondary:
+      'bg-bjgray-100 hover:bg-bjgray-200 active:bg-bjgray-200 focus:bg-bjgray-200 focus:outline-none',
   };
 
   return (
-    <button className={buttonStyle.className} type={type} disabled={disabled}>
-      {children}
-    </button>
+    <>
+      <button
+        className={`${buttonStyle[className]} w-full h-[64px] rounded-5xl text-b-1-medium text-bjblack disabled:bg-bjgray-300 disabled:text-bjgray-500`}
+        type={type}
+        disabled={disabled}
+      >
+        {children}
+      </button>
+    </>
   );
 }
 
 export default MainButton;
 
 MainButton.propTypes = {
+  className: string,
   type: string,
   disabled: bool,
   children: string,
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,4 +1,9 @@
-import { MainButton } from '@/components/Atoms';
+import {
+  MainButton,
+  ModalButton,
+  OutlineButton,
+  SmallButton,
+} from '@/components/Atoms';
 import { SignUp } from '@/pages/index';
 import { Link } from 'react-router-dom';
 import { createBrowserRouter } from 'react-router-dom';
@@ -11,9 +16,34 @@ const routes = [
   {
     path: '/atoms',
     element: (
-      <MainButton disabled type="button">
-        다음
-      </MainButton>
+      <>
+        <MainButton type="button" className="large_primary">
+          버튼
+        </MainButton>
+        <MainButton type="button" className="large_secondary">
+          버튼
+        </MainButton>
+        <MainButton disabled type="button">
+          버튼
+        </MainButton>
+        <ModalButton type="button" className="large_primary">
+          버튼
+        </ModalButton>
+        <ModalButton type="button" className="large_secondary">
+          버튼
+        </ModalButton>
+        <ModalButton disabled type="button">
+          버튼
+        </ModalButton>
+        <OutlineButton type="button">버튼</OutlineButton>
+        <OutlineButton disabled type="button">
+          버튼
+        </OutlineButton>
+        <SmallButton type="button">버튼</SmallButton>
+        <SmallButton disabled type="button">
+          버튼
+        </SmallButton>
+      </>
     ),
   },
   {
