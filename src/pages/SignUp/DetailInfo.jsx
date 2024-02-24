26e8140cf69946bf0e973bf0df565b83456e4826
jio <pennicillium@daum.net>
2024-02-25 01:27:16 +0900

    [Feat] 회원가입 페이지 - 닉네임 중복 확인 기능 구현 #9

--- src/pages/SignUp/DetailInfo.jsx
+++ src/pages/SignUp/DetailInfo.jsx
@@ -2,16 +2,19 @@ import { Link, Form, useLocation, redirect } from 'react-router-dom';
 import { MainButton } from '@/components/Atoms';
 import { useState, useEffect } from 'react';
 import { useDebounce } from '@/hooks/index';
+import Pocketbase from 'pocketbase';
 /*
 1. 만약 signup/detail로 path를 갖게 된다면 basicInfo가 없을시 basicInfo쪽으로 넘겨야됨
-2. 닉네임 중복 확인
-3. 휴대폰 중복 확인 
+2. 닉네임 중복 확인 , 닉네임 유효성 검사 ? ,,,
+3. 휴대폰 중복 확인 , 휴대폰 유효성검사 .,... 
 4. 
 */
+const pb = new Pocketbase(import.meta.env.VITE_PB_URL);
 
 export default function DetailInfo() {
   const { state } = useLocation();
   const [userInfo, setUserInfo] = useState(state);
+  const [isValidateNickname, setIsValidateNickname] = useState(false);
   const debouncedNickname = useDebounce(userInfo.nickname, 500);
   const debouncedPhone = useDebounce(userInfo.phone, 500);
 
@@ -24,13 +27,27 @@ export default function DetailInfo() {
   };
 
   const handleBirth = (e) => {
-    console.log(e.target.value);
     setUserInfo((prev) => ({ ...prev, birth: e.target.value }));
   };
 
   const handleGender = (e) => {
     setUserInfo((prev) => ({ ...prev, gender: e.target.id }));
   };
+
+  useEffect(() => {
+    if (state && userInfo.nickname !== '') {
+      const USER_API = `${import.meta.env.VITE_PB_URL}api/collections/users/records?filter=(nickname="${debouncedNickname}")`;
+      fetch(USER_API)
+        .then((res) => res.json())
+        .then((data) => {
+          return setIsValidateNickname(data.items.length === 0);
+        })
+        .catch((error) => console.error(error));
+    }
+  }, [debouncedNickname]);
+
+  useEffect(() => {}, [debouncedPhone]);
+
   if (state) {
     return (
       <>
@@ -38,7 +55,17 @@ export default function DetailInfo() {
         <h2>상세 정보</h2>
         <Form className="flex flex-col" action="">
           <label htmlFor="nickname">닉네임</label>
-          <input type="text" id="nickname" onChange={handleNickname} />
+          <input
+            type="text"
+            id="nickname"
+            maxLength="10"
+            onChange={handleNickname}
+          />
+          {userInfo.nickname == '' || isValidateNickname ? (
+            ''
+          ) : (
+            <p>중복된 닉네임은 사용할 수 없어용 </p>
+          )}
           <label htmlFor="phone">휴대폰</label>
           <input type="text" id="phone" onChange={handlePhone} />
           <label htmlFor="birth">생년월일</label>
@@ -49,6 +76,7 @@ export default function DetailInfo() {
             onChange={handleBirth}
           />
           <fieldset>
+            <legend>성별</legend>
             <label htmlFor="male">남자</label>
             <input
               type="radio"
@@ -73,6 +101,6 @@ export default function DetailInfo() {
       </>
     );
   } else {
-    return redirect('/signup');
+    redirect('/signup');
   }
 }
