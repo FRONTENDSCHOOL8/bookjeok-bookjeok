9b4606d18a7fff9099452dc023654e1bc9f90536
jio <pennicillium@daum.net>
2024-02-25 01:54:08 +0900

    [Feat] read 기능 수행하는 비동기 유틸함수 생성 - sdk 가 안돼서 일단 REST API로 만들었음 #9

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,3 @@
+export { default as fetchReadDataAPI } from './fetchReadDataAPI';
 export { default as validatePassword } from './validatePassword';
 export { default as validateEmail } from './validateEmail';
