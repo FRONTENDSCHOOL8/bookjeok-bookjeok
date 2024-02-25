d8aa189e1b9b5b1bb32a451d4a25935102dea786
jio <pennicillium@daum.net>
2024-02-25 14:38:07 +0900

    [Feat] 회원가입 페이지 error페이지 추가, welcome페이지 path 추가 #9

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,10 +1,11 @@
 import AtomMaking from '@/AtomMaking';
 import Erro404 from '@/pages/Error404/Erro404';
-import { Intro, SignUp } from '@/pages/index';
+import { Intro, SignUp, Welcome } from '@/pages/index';
 import { createBrowserRouter } from 'react-router-dom';
 
 import BasicInfo from '../pages/SignUp/BasicInfo';
 import DetailInfo from '../pages/SignUp/DetailInfo';
+
 const routes = [
   {
     path: '/',
@@ -23,6 +24,12 @@ const routes = [
   {
     path: '/signup/detail',
     element: <DetailInfo />,
+    errorElement: <Erro404 />,
+  },
+  {
+    path: '/Welcome',
+    element: <Welcome />,
+    errorElement: <Erro404 />,
   },
 ];
 
