d84bc0fc9dc0f9a3fb9ecf65cdaa3707ed04703a
yuna <rnssue@naver.com>
2024-02-25 21:59:53 +0900

    [Modify] 버튼 컴포넌트 수정

--- src/components/Atoms/Buttons/ModalButton/ModalButton.jsx
+++ src/components/Atoms/Buttons/ModalButton/ModalButton.jsx
@@ -1,11 +1,11 @@
 import { string, bool } from 'prop-types';
 
-function ModalButton({ className, type, disabled, children }) {
+function ModalButton({ className, type, disabled, children, ...rest }) {
   const buttonStyle = {
     large_primary:
-      'bg-bjyellow-400 hover:bg-bjyellow-500 active:bg-bjyellow-500 focus:bg-bjyellow-500 focus:outline-none',
+      'bg-bjyellow-400 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
     large_secondary:
-      'bg-bjgray-100 hover:bg-bjgray-200 active:bg-bjgray-200 focus:bg-bjgray-200 focus:outline-none',
+      'bg-bjgray-100 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
   };
 
   return (
@@ -14,6 +14,7 @@ function ModalButton({ className, type, disabled, children }) {
         className={`${buttonStyle[className]} w-full h-[64px] rounded-5xl text-b-1-medium text-bjblack disabled:bg-bjgray-300 disabled:text-bjgray-500`}
         type={type}
         disabled={disabled}
+        {...rest}
       >
         {children}
       </button>
--- src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
+++ src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
@@ -1,13 +1,18 @@
 import { string, bool } from 'prop-types';
 
-function SmallButton({ type, disabled, children }) {
+function SmallButton({ type, disabled, children, ...rest }) {
   const buttonStyle = {
     className:
-      'h-[36px] rounded-xl px-4 text-b-2-medium text-bjblack bg-bjyellow-400 hover:bg-bjyellow-500 active:bg-bjyellow-500 focus:bg-bjyellow-500 focus:outline-none disabled:bg-bjgray-300 disabled:text-bjgray-500',
+      'h-[36px] rounded-xl px-4 text-b-2-medium text-bjblack bg-bjyellow-400 disabled:bg-bjgray-300 disabled:text-bjgray-500 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
   };
 
   return (
-    <button className={buttonStyle.className} type={type} disabled={disabled}>
+    <button
+      className={buttonStyle.className}
+      type={type}
+      disabled={disabled}
+      {...rest}
+    >
       {children}
     </button>
   );
