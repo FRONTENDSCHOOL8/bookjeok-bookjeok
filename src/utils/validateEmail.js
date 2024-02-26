bd217ca18a479c82978876c0fa8cc576c5309cb6
jio <pennicillium@daum.net>
2024-02-26 14:19:55 +0900

    [Moidify] 이메일 유효 검사 유틸함수 수정 #9

--- src/utils/validateEmail.js
+++ src/utils/validateEmail.js
@@ -3,7 +3,7 @@ const validateEmail = (email) => {
     /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
   if (emailReg.test(email)) return true;
   else {
-    return '올바른 이메일 형식이 아닙니다. ';
+    return false;
   }
 };
 export default validateEmail;
