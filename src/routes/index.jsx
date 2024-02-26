d9213c0af09f68e2650b27aded027f72aa22c059
clapsheep <sktndid1203@naver.com>
2024-02-26 10:08:17 +0900

    [Fix] 병합 후 오류 해결

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,35 +1,13 @@
 import Erro404 from '@/pages/Error404/Erro404';
-
 import { createBrowserRouter } from 'react-router-dom';
-
-import BasicInfo from '../pages/SignUp/BasicInfo';
-import DetailInfo from '../pages/SignUp/DetailInfo';
+import { RootLayout } from '@/pages';
+import routingPages from './routingPages';
 
 const routes = [
   {
     path: '/',
     element: <RootLayout />,
     errorElement: <Erro404 />,
-  },
-  {
-    path: '/atomMaking',
-    element: <AtomMaking />,
-  },
-  {
-    path: '/signup',
-    element: <BasicInfo />,
-    children: [],
-  },
-  {
-    path: '/signup/detail',
-    element: <DetailInfo />,
-    errorElement: <Erro404 />,
-  },
-  {
-    path: '/Welcome',
-    element: <Welcome />,
-    errorElement: <Erro404 />,
-
     children: routingPages,
   },
 ];
