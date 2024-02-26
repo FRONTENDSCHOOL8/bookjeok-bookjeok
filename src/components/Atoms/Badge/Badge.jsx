1fc045854c59365fa69d8d6d8a43d2a8448020db
clapsheep <sktndid1203@naver.com>
2024-02-26 18:09:10 +0900

    [Fix] Badge 컴포넌트 수정
    - className 받을 수 있게 수정

--- src/components/Atoms/Badge/Badge.jsx
+++ src/components/Atoms/Badge/Badge.jsx
@@ -1,16 +1,19 @@
 import { string } from 'prop-types';
 
-function Badge({ children }) {
+function Badge({ className, children }) {
   const badgeStyle = {
     className:
-      'min-w-20 h-[24px] inline-block text-center rounded-xl px-2 py-0.5 text-b-2-medium text-bjblack bg-bjyellow-400',
+      'h-[24px] inline-block text-center rounded-xl px-2 opacity-90 py-0.5 text-b-2-medium text-bjblack bg-bjyellow-400',
   };
 
-  return <span className={badgeStyle.className}>{children}</span>;
+  return (
+    <span className={`${badgeStyle.className} ${className}`}>{children}</span>
+  );
 }
 
 export default Badge;
 
 Badge.propTypes = {
+  className: string,
   children: string,
 };
