39c05f48a43c4a326dd622175c72aa59c5ed2eff
yuna <rnssue@naver.com>
2024-02-27 10:05:56 +0900

    [Modify] 체크박스 컴포넌트 수정

--- src/components/Atoms/Inputs/CheckboxForm/CheckboxForm.jsx
+++ src/components/Atoms/Inputs/CheckboxForm/CheckboxForm.jsx
@@ -1,15 +1,24 @@
 import { string, bool } from 'prop-types';
 
-function CheckboxForm({ id, name, value, checked, children, ...rest }) {
+function CheckboxForm({
+  className,
+  id,
+  name,
+  value,
+  checked,
+  children,
+  ...rest
+}) {
   const checkboxFormStyle = {
-    className: 'flex flex-row flex-nowrap justify-between items-center gap-4',
+    className:
+      'flex flex-row flex-nowrap justify-between items-center gap-4 h-full',
     classNameLabelText: 'text-b-2-regular text-bjblack',
     classNameInput:
       'flex-shrink-0 appearance-none w-[24px] h-[24px] rounded-full bg-bjgray-300 checked:bg-bjyellow-400 checked:bg-[url("/src/assets/icons/checkmark.svg")] checked:bg-[length:16px_16px] checked:bg-no-repeat checked:bg-center focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
   };
 
   return (
-    <div className={checkboxFormStyle.className}>
+    <div className={`${checkboxFormStyle.className} ${className}`}>
       <label htmlFor={id} className={checkboxFormStyle.classNameLabelText}>
         {children}
       </label>
@@ -29,6 +38,7 @@ function CheckboxForm({ id, name, value, checked, children, ...rest }) {
 export default CheckboxForm;
 
 CheckboxForm.propTypes = {
+  className: string,
   id: string,
   name: string,
   value: string,
