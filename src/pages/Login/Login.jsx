5fa0fea0f993a527c08993d4810bea61e15174ec
jio <pennicillium@daum.net>
2024-02-27 16:41:44 +0900

    [Chore] 로그인 페이지 수정 - 불필요한 import 삭제 - 로직 구조 주석 추가 #6

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,13 +2,14 @@ import { getDocumentTitle } from '@/utils';
 import { Helmet } from 'react-helmet-async';
 import { TextForm, NomalTitle, MainButton } from '@/components/Atoms';
 import { useRef } from 'react';
-import { useDebounce } from '@/hooks';
 import pb from '@/api/pocketbase';
 
 /*
-1. useRef로 email, password -> 상태관리x
-2. 버튼을 눌렀을때 이메일과 패스워드가 맞는지 확인 
+1. useRef로 email, password -> useRef 상태관리x 
+2. 버튼을 눌렀을때 이메일과 패스워드가 맞는지 확인 => 완료  
 3. 어떻게 로그인 성공 / 실패를 알려야될까.. 고민중. . .
+  - 시각적 에니메이션 -> 어떻게 스크린리더사용자에게 전달할수 있을까?
+  - 성공 알림 모달-> 확인 버튼 클릭시 메인으로 이동 | 실패 모달 ?  
 */
 
 function Login() {
@@ -23,6 +24,7 @@ function Login() {
         : (passwordRef.current = e.target.value);
     }
   };
+  // 로그인 이벤트 함수 (로그인 성공/실패 결과 표시 필요 ! )
   const handleLogin = () => {
     pb.collection('users')
       .authWithPassword(`${emailRef.current}`, `${passwordRef.current}`)
@@ -37,20 +39,10 @@ function Login() {
       </Helmet>
       <NomalTitle backButton>로그인</NomalTitle>
       <div className="flex flex-grow flex-col " onChange={handleLoginForm}>
-        <TextForm
-          type="email"
-          placeholder="email@email.com"
-          description="description"
-          name="email"
-        >
+        <TextForm type="email" placeholder="email@email.com" name="email">
           이메일
         </TextForm>
-        <TextForm
-          type="password"
-          placeholder=""
-          description="description"
-          name="password"
-        >
+        <TextForm type="password" placeholder="" name="password">
           비밀번호
         </TextForm>
       </div>
