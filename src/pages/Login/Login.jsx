829c8c8b510142c95bb94e34199eb4aaaebbd3f2
jio <pennicillium@daum.net>
2024-02-27 14:25:10 +0900

    [Feat] 로그인 페이지 마크업 스타일링 #6

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,30 @@
 import { getDocumentTitle } from '@/utils';
 import { Helmet } from 'react-helmet-async';
+import { TextForm, NomalTitle, MainButton } from '@/components/Atoms';
 
 function Login() {
   return (
-    <>
+    <div className="box-border flex h-screen  flex-grow flex-col justify-center px-4 ">
       <Helmet>
         <title>{getDocumentTitle('로그인')}</title>
       </Helmet>
-      <div>Login</div>
-    </>
+      <NomalTitle backButton>로그인</NomalTitle>
+      <div className="flex flex-grow flex-col ">
+        <TextForm
+          type="email"
+          placeholder="email@email.com"
+          description="description"
+        >
+          이메일
+        </TextForm>
+        <TextForm type="password" placeholder="" description="description">
+          비밀번호
+        </TextForm>
+      </div>
+      <MainButton className="my-16" type="button">
+        로그인
+      </MainButton>
+    </div>
   );
 }
 
