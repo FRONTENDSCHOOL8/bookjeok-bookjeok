ff03cd6a11d7ef2efcb29053e552e4b5fbd71933
yuna <rnssue@naver.com>
2024-02-28 12:05:56 +0900

    [Modify] 아토믹 컴퍼넌트 className 수정

--- src/components/Atoms/Buttons/OutlineButton/OutlineButton.jsx
+++ src/components/Atoms/Buttons/OutlineButton/OutlineButton.jsx
@@ -1,16 +1,23 @@
 import { oneOfType, string, elementType } from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function OutlineButton({ as: ComponentName = 'Link', ...rest }) {
+function OutlineButton({ as: ComponentName = Link, className, ...rest }) {
   const buttonStyle = {
     className:
       'flex justify-center items-center w-full h-[64px] rounded-5xl text-b-1-medium text-bjblack border-2 border-bjblack disabled:text-bjgray-500 disabled:border-bjgray-500 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
   };
 
-  return <ComponentName className={buttonStyle.className} {...rest} />;
+  return (
+    <ComponentName
+      className={`${buttonStyle.className} ${className}`}
+      {...rest}
+    />
+  );
 }
 
 export default OutlineButton;
 
 OutlineButton.propTypes = {
   as: oneOfType([string, elementType]),
+  className: string,
 };
--- src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
+++ src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
@@ -1,15 +1,22 @@
 import { oneOfType, string, elementType } from 'prop-types';
+import { Link } from 'react-router-dom';
 
-function SmallButton({ as: ComponentName = 'Link', ...rest }) {
+function SmallButton({ as: ComponentName = Link, className, ...rest }) {
   const buttonStyle = {
     className:
       'flex justify-center items-center h-[36px] rounded-xl px-4 text-b-2-medium text-bjblack bg-bjyellow-400 disabled:bg-bjgray-300 disabled:text-bjgray-500 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
   };
 
-  return <ComponentName className={buttonStyle.className} {...rest} />;
+  return (
+    <ComponentName
+      className={`${buttonStyle.className} ${className}`}
+      {...rest}
+    />
+  );
 }
 
 export default SmallButton;
 SmallButton.propTypes = {
   as: oneOfType([string, elementType]),
+  className: string,
 };
