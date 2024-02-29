85c9792e5ff74a0f30da853ba832dcc2c6d4d746
jio <pennicillium@daum.net>
2024-02-29 16:13:28 +0900

    [Modify] Atoms- NomalTitle, RadioForm 마크업수정 #41

--- src/components/Atoms/Title/NomalTitle/NomalTitle.jsx
+++ src/components/Atoms/Title/NomalTitle/NomalTitle.jsx
@@ -1,5 +1,4 @@
 import { bool, string } from 'prop-types';
-import { Link } from 'react-router-dom';
 import { Svg } from '@/components/Atoms';
 
 function NomalTitle({
@@ -8,7 +7,6 @@ function NomalTitle({
   children,
   subText,
   resetButton = false,
-  path,
 }) {
   const nomalTitleStyle = {
     className: 'h-[56px] px-4 flex flex-row justify-center items-center',
@@ -18,9 +16,13 @@ function NomalTitle({
 
   if (backLink) {
     backLinkElement = (
-      <Link to={path} title="뒤로 가기" aria-label="뒤로 가기">
+      <a
+        onClick={() => window.history.back()}
+        title="뒤로 가기"
+        aria-label="뒤로 가기"
+      >
         <Svg id="arrow-left" />
-      </Link>
+      </a>
     );
   }
 
@@ -40,7 +42,7 @@ function NomalTitle({
         {backLinkElement}
       </div>
       <div className="flex flex-1 flex-grow flex-col items-center justify-center whitespace-nowrap">
-        <span className="text-b-1-medium text-bjblack">{children}</span>
+        <h1 className="text-b-1-medium text-bjblack">{children}</h1>
         <span className="text-b-3-medium text-bjgray-500">{subText}</span>
       </div>
       <div className="flex flex-1 items-center justify-end">
@@ -58,5 +60,4 @@ NomalTitle.propTypes = {
   children: string,
   subText: string,
   resetButton: bool,
-  path: string,
 };
