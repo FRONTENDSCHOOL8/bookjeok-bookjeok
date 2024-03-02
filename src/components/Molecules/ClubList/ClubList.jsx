b8fb315c7273bdb8b772247535b361cbcff0a3b8
jio <pennicillium@daum.net>
2024-03-02 17:48:41 +0900

    [Feat] ClubList 컴포넌트 생성 #8

--- src/components/Molecules/ClubList/ClubList.jsx
+++ src/components/Molecules/ClubList/ClubList.jsx
@@ -1,5 +1,24 @@
-function ClubList() {
-  return <div>ClubList</div>;
+import { string } from 'prop-types';
+
+function ClubList({ title, img, schedule }) {
+  return (
+    <li className="flex justify-between bg-bjgray-100">
+      <figcaption className="flex flex-col justify-around p-[12px]">
+        <p className="text-h-2-semibold">{title}</p>
+        <span className="text-b-3-light text-bjgray-500">{schedule}</span>
+      </figcaption>
+      <figure>
+        <img className="h-[100px] w-[86px]" src={img} alt="" />
+      </figure>
+    </li>
+  );
 }
 
 export default ClubList;
+ClubList.propTypes = {
+  title: string.isRequired,
+  img: string,
+  schedule: string.isRequired,
+};
+
+ClubList.displayName = 'ClubList';
