c225307e84ec69c0b5276fda8281cf68f3a5df8e
jio <pennicillium@daum.net>
2024-03-02 17:28:24 +0900

    [Feat] 나의 모임 리스트 페이지 마크업 #8

--- src/pages/MyClubList/MyClubList.jsx
+++ src/pages/MyClubList/MyClubList.jsx
@@ -1,6 +1,14 @@
 import { getDocumentTitle } from '@/utils';
 import { Helmet } from 'react-helmet-async';
 import { ClubList } from '@/components/Molecules';
+import { NomalTitle, ThinTextForm } from '@/components/Atoms';
+/*
+1. socialing db에서 applicant가 사용자인 경우, 
+  creator가 나인 경우를 분리하여 렌더링
+2. 3개이상인 경우 더보기 버튼 다음 
+
+*/
+
 function MyClubList() {
   return (
     <>
@@ -8,8 +16,18 @@ function MyClubList() {
         <title>{getDocumentTitle('나의 모임 리스트')}</title>
       </Helmet>
       <div>
-        dd
-        <ClubList>g</ClubList>
+        <NomalTitle backLink={true}>모임 리스트</NomalTitle>
+        <ThinTextForm backLink={true} type="search" placeholder="search" />
+        <ul>
+          <p>참여중인 모임</p>
+          <ClubList src=""></ClubList>
+          <button>더 보기</button>
+        </ul>
+        <ul>
+          <p>내가 만든 모임</p>
+          <ClubList src=""></ClubList>
+          <button>더 보기</button>
+        </ul>
       </div>
     </>
   );
