07c778dc72f12377a222123e37d81d91e62712bf
jio <pennicillium@daum.net>
2024-03-03 04:01:59 +0900

    [Feat] 참여중인 모임, 내가 만든 모임 렌더링 기능 구현 #8

--- src/pages/MyClubList/MyClubList.jsx
+++ src/pages/MyClubList/MyClubList.jsx
@@ -2,14 +2,46 @@ import { getDocumentTitle } from '@/utils';
 import { Helmet } from 'react-helmet-async';
 import { ClubList } from '@/components/Molecules';
 import { NomalTitle, ThinTextForm } from '@/components/Atoms';
+import useUserInfoStore from '@/store/useUserInfoStore';
+import { useEffect, useState } from 'react';
+import pb from '@/api/pocketbase';
+import { getPbImgs } from '@/utils';
 /*
 1. socialing db에서 applicant가 사용자인 경우, 
   creator가 나인 경우를 분리하여 렌더링
-2. 3개이상인 경우 더보기 버튼 다음 
+2. 3개이상인 경우 더보기 버튼 =>  
 
+3. 
 */
 
 function MyClubList() {
+  const { userInfo } = useUserInfoStore();
+  const [createdClub, setCreatedClub] = useState([]);
+  const [confirmedClub, setConfirmedClub] = useState([]);
+
+  useEffect(() => {
+    const fetchPb = async () => {
+      try {
+        pb.autoCancellation(false);
+        const data = (
+          await pb.collection('socialing').getList(1, 10, {
+            filter: `createUser = "${userInfo.id}" || confirmUser ?~ "${userInfo.id}" `,
+          })
+        ).items;
+        data.forEach((item) => {
+          if (item.createUser === userInfo.id) {
+            setCreatedClub((prev) => [...prev, item]);
+          } else {
+            setConfirmedClub((prev) => [...prev, item]);
+          }
+        });
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchPb();
+  }, [userInfo]);
+
   return (
     <>
       <Helmet>
@@ -20,13 +52,27 @@ function MyClubList() {
         <ThinTextForm backLink={true} type="search" placeholder="search" />
         <ul>
           <p>참여중인 모임</p>
-          <ClubList src=""></ClubList>
-          <button>더 보기</button>
+          {confirmedClub.map((item) => (
+            <ClubList
+              key={item.id}
+              title={item.title}
+              schedule={item.dateTime}
+              img={getPbImgs(item)}
+            ></ClubList>
+          ))}
+          {confirmedClub.length > 3 ? <button>더 보기</button> : ''}
         </ul>
         <ul>
           <p>내가 만든 모임</p>
-          <ClubList src=""></ClubList>
-          <button>더 보기</button>
+          {createdClub.map((item) => (
+            <ClubList
+              key={item.id}
+              title={item.title}
+              schedule={item.dateTime}
+              img={getPbImgs(item)}
+            ></ClubList>
+          ))}
+          {confirmedClub.length > 3 ? <button>더 보기</button> : ''}
         </ul>
       </div>
     </>
