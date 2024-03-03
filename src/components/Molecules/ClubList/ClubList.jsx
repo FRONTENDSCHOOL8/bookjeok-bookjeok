e8f90924ec372952585d59dd6897a11429d6dd92
jio <pennicillium@daum.net>
2024-03-03 17:17:11 +0900

    [Modify] ClubList 컴포넌트 수정 #8 - Link 추가 , 스타일링 수정

--- src/components/Molecules/ClubList/ClubList.jsx
+++ src/components/Molecules/ClubList/ClubList.jsx
@@ -1,21 +1,35 @@
 import { string } from 'prop-types';
-
-function ClubList({ title, img, schedule }) {
+import { Link } from 'react-router-dom';
+function ClubList({ id, title, img, schedule }) {
   return (
-    <li className="flex justify-between bg-bjgray-100">
-      <figcaption className="flex flex-col justify-around p-[12px]">
-        <p className="text-h-2-semibold">{title}</p>
-        <span className="text-b-3-light text-bjgray-500">{schedule}</span>
-      </figcaption>
-      <figure>
-        <img className="h-[100px] w-[86px]" src={img} alt="" />
-      </figure>
+    <li className="bg-bjgray-100">
+      <Link
+        className="flex h-[100px] w-full flex-row overflow-hidden rounded-5xl bg-bjgray-100"
+        to={`/mainClub/${id}`}
+      >
+        <figcaption className="flex flex flex-grow flex-col justify-between  p-3 ">
+          <p className="line-clamp-1 text-h-2-semibold text-h-2-semibold leading-9 text-bjblack">
+            {title}
+          </p>
+          <span className=" text-b-3-light leading-9 text-bjgray-500">
+            {schedule}
+          </span>
+        </figcaption>
+        <figure className="shrink-0 basis-20">
+          <img
+            className="h-[100px] w-[86px] w-full object-cover"
+            src={img}
+            alt=""
+          />
+        </figure>
+      </Link>
     </li>
   );
 }
 
 export default ClubList;
 ClubList.propTypes = {
+  id: string.isRequired,
   title: string.isRequired,
   img: string,
   schedule: string.isRequired,
