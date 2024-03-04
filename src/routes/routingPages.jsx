821ef768dfa63caf5d7af33d77521d3a06aae678
jio <pennicillium@daum.net>
2024-03-04 15:32:38 +0900

    [Modify] routes 수정

--- src/routes/routingPages.jsx
+++ src/routes/routingPages.jsx
@@ -14,6 +14,7 @@ import {
   MyPage,
   Welcome,
   MyClubList,
+  DetailBookReview,
 } from '@/pages';
 import ApplicationClub1, {
   loader as ApplicationClub1Loader,
@@ -65,6 +66,14 @@ const routingPages = [
     element: (
       <ProtectRoute>
         <MainBookReview />
+      </ProtectRoute>
+    ),
+  },
+  {
+    path: '/mainBookReview/:bookreviewId',
+    element: (
+      <ProtectRoute>
+        <DetailBookReview />
       </ProtectRoute>
     ),
   },
