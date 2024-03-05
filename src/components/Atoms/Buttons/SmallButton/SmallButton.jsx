ea3d08517a9af3665c083281839ad8111f2654f8
yuna <rnssue@naver.com>
2024-03-05 09:14:36 +0900

    [Design] 버튼 스타일링 수정

--- src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
+++ src/components/Atoms/Buttons/SmallButton/SmallButton.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 function SmallButton({ as: ComponentName = Link, className, ...rest }) {
   const buttonStyle = {
     className:
-      'flex justify-center items-center h-[36px] rounded-xl px-4 text-b-2-medium text-bjblack bg-bjyellow-400 disabled:bg-bjgray-300 disabled:text-bjgray-500 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/10',
+      'flex justify-center items-center h-[36px] rounded-xl px-4 text-b-2-medium text-bjblack bg-bjyellow-400 disabled:bg-bjgray-300 disabled:text-bjgray-500 focus:outline-none focus-visible:ring focus-visible:ring-bjblack/30',
   };
 
   return (
