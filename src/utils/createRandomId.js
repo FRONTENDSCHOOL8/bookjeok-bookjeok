05352cefd8bf11db2a7dd10b4fffb0df4c0b957f
jio <pennicillium@daum.net>
2024-03-07 11:50:20 +0900

    [Feat]랜덤 id 생성 유틸함수

--- src/utils/createRandomId.js
+++ src/utils/createRandomId.js
@@ -1,5 +1,12 @@
 function createRandomId() {
-  return crypto.randomUUID().replaceAll('-', '').slice(0, 15);
+  // return crypto.randomUUID().replaceAll('-', '').slice(0, 15);
+  const randomChar =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const resultArray = [];
+  for (let i = 0; i < 15; i++) {
+    resultArray.push(randomChar[~~(Math.random() * randomChar.length)]);
+  }
+  return resultArray.join('');
 }
 
 export default createRandomId;
