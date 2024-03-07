e7ed9fecf44a4f8ce2aa10e04b4049d642652296
jio <pennicillium@daum.net>
2024-03-07 14:07:52 +0900

    [Feat] 마이페이지 독후감 렌더링 기능 #58

--- src/pages/MyPage/MyPage.jsx
+++ src/pages/MyPage/MyPage.jsx
@@ -1,11 +1,11 @@
 import { Accordion, NomalTitle } from '@/components/Atoms';
-import { Avatar, ClubList, GNB } from '@/components/Molecules';
+import { Avatar, ClubList, GNB, BookReviewList } from '@/components/Molecules';
 import { getDocumentTitle, getPbImgs } from '@/utils';
 import { Helmet } from 'react-helmet-async';
 import useUserInfoStore from '@/store/useUserInfoStore';
 import { useEffect, useState } from 'react';
 import pb from '@/api/pocketbase';
-
+import { Link } from 'react-router-dom';
 /*
 1. userInfo에 있는 createSocialing, participantSocialing 
   배열에 있는 모임 id를 어떻게 가져올까...
@@ -13,17 +13,19 @@ import pb from '@/api/pocketbase';
   렌더링할때 creteSocialing에 모임 id가 있음 만든모임에 두고 confirm user에 있으면 참여한 모임으로 렌더링하구 
 
 2. 로그아웃 기능 구현   
-  useUserInfoStore에서 deleteUserInfo
+  useUserInfoStore에서 clearUserInfo
 3. 프로필 사진 수정 기능 구현
+4. GNB 고정
+5. 독후감 
+
+
 
 */
 export function MyPage() {
-  const {
-    userInfo: { id, nickname, img },
-    clearUserInfo,
-  } = useUserInfoStore((state) => state);
-
+  const { userInfo, clearUserInfo } = useUserInfoStore((state) => state);
+  console.log(userInfo);
   const [clubData, setClubData] = useState(null);
+  const [bookReviewData, setBookReviewData] = useState();
   const handleLogout = () => {
     clearUserInfo();
     pb.authStore.clear();
@@ -31,7 +33,7 @@ export function MyPage() {
 
   useEffect(() => {
     pb.collection('users')
-      .getOne(`${id}`, {
+      .getOne(`${userInfo.id}`, {
         expand: 'createSocialing, participantSocialing',
       })
       .then((data) => {
@@ -39,8 +41,17 @@ export function MyPage() {
         setClubData(data.expand);
       })
       .catch((Error) => console.error(Error));
-  }, [id]);
-  console.log(clubData);
+  }, [userInfo.id]);
+  console.log(bookReviewData);
+
+  useEffect(() => {
+    pb.collection('bookReview')
+      .getFullList({
+        filter: `writer ="${userInfo.id}"`,
+      })
+      .then((data) => setBookReviewData(data));
+  }, [userInfo.id]);
+
   return (
     <>
       <Helmet>
@@ -53,44 +64,69 @@ export function MyPage() {
         <main className="flex flex-col px-4">
           <div className="relative mb-5 mt-12">
             <Avatar
-              nickName={nickname}
-              text="나의 활동"
+              nickName={userInfo.nickname}
+              src={getPbImgs(userInfo)}
               className="relative !top-0"
             ></Avatar>
           </div>
-          <hr />
 
           {clubData && (
             <>
               <Accordion open mainText="참여중인 모임" className="mb-4">
                 <ul className="flex flex-col gap-y-4">
-                  {clubData.participantSocialing.map(
-                    ({ collectionId, id, title, img }) => (
-                      <ClubList
-                        key={id}
-                        id={id}
-                        title={title}
-                        schedule="3.16(토) 오후 2:00"
-                        img={getPbImgs({ collectionId, id, img })}
-                      />
-                    )
-                  )}
+                  {clubData.participantSocialing.map((item) => (
+                    <ClubList
+                      key={item.id}
+                      id={item.id}
+                      title={item.title}
+                      schedule="3.16(토) 오후 2:00"
+                      src={getPbImgs(item)}
+                    />
+                  ))}
                 </ul>
               </Accordion>
-
+              <hr />
               <Accordion open mainText="주최중인 모임" className="mb-4">
                 <ul className="flex flex-col gap-y-4">
-                  {clubData.createSocialing.map(
-                    ({ collectionId, id, title, img }) => (
-                      <ClubList
-                        key={id}
-                        id={id}
-                        title={title}
-                        schedule="3.16(토) 오후 2:00"
-                        img={getPbImgs({ collectionId, id, img })}
-                      />
-                    )
-                  )}
+                  {clubData.createSocialing.map((item) => (
+                    <ClubList
+                      key={item.id}
+                      id={item.id}
+                      title={item.title}
+                      schedule="3.16(토) 오후 2:00"
+                      img={getPbImgs(item)}
+                    />
+                  ))}
+                </ul>
+              </Accordion>
+              <hr />
+              <Accordion open mainText="내가 쓴 독후감">
+                <ul className="flex flex-col gap-5 px-1">
+                  {bookReviewData.map((item) => (
+                    <Link
+                      className="boreder-b-1"
+                      key={item.id}
+                      to={`/mainBookReview/${item.id}`}
+                    >
+                      <div className="my-[7px] flex items-center gap-x-2">
+                        <div>
+                          <p className="line-clamp-1 text-b-0-regular text-bjblack">
+                            {item.title}
+                          </p>
+                          <p className="line-clamp-2 text-b-2-regular text-bjgray-500">
+                            {item.detail}
+                          </p>
+                        </div>
+                        <div className="ml-auto shrink-0">
+                          <img
+                            src={getPbImgs(item)}
+                            alt={item.title}
+                            className="aspect-square w-[54px] rounded-4xl object-cover"
+                          />
+                        </div>
+                      </div>
+                    </Link>
+                  ))}
                 </ul>
               </Accordion>
             </>
