29042ba6c304a2891e8c0e6c1f7f231acfaaac6e
jio <pennicillium@daum.net>
2024-03-08 23:13:25 +0900

    [Feat]프로필 수정 페이지 마크업 #86

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -1,12 +1,49 @@
 import { Helmet } from 'react-helmet-async';
 import { getDocumentTitle } from '@/utils';
+import {
+  NomalTitle,
+  ImageForm,
+  TextForm,
+  MainButton,
+} from '@/components/Atoms';
+import { Form } from 'react-router-dom';
 export function EditProfile() {
   return (
     <>
       <Helmet>
         <title>{getDocumentTitle('프로필 수정')}</title>
       </Helmet>
-      <div>프로필 수정</div>
+      <div className="relative flex h-svh w-full flex-col  ">
+        <NomalTitle backLink path="/myPage">
+          프로필 수정
+        </NomalTitle>
+        <Form className="flex flex-col gap-4 px-4">
+          <span>프로필 사진</span>
+          <ImageForm id="img" src="" />
+          <TextForm type="text" id="nickname" name="nickname">
+            닉네임
+          </TextForm>
+          <TextForm
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="off"
+          >
+            비밀번호
+          </TextForm>
+          <TextForm
+            id="passwordConfirm"
+            type="password"
+            name="passwordConfirm"
+            autoComplete="off"
+          >
+            비밀번호 확인
+          </TextForm>
+        </Form>
+        <div className="mt-auto p-4">
+          <MainButton as="button">저장</MainButton>
+        </div>
+      </div>
     </>
   );
 }
