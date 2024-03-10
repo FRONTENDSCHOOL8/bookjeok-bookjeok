e2da2b544bf59b917a1a34cf58a0cd9415901a79
yuna <rnssue@naver.com>
2024-03-10 17:08:21 +0900

    [Style] ChatRoom, MessageBubble 수정

--- src/components/Atoms/MessageBubble/MessageBubble.jsx
+++ src/components/Atoms/MessageBubble/MessageBubble.jsx
@@ -9,7 +9,7 @@ function MessageBubble({ align = 'left', src, alt, nickname, children, time }) {
 
   return (
     <>
-      <li className={`${messageBubbleStyle[align]}  group`}>
+      <li className={`${messageBubbleStyle[align]} group`}>
         <div className="flex flex-row items-center gap-4 group-[.group-right]:flex-row-reverse group-[.group-right]:text-right">
           <div className="self-start pt-3">
             <RoundImage src={src} alt={alt} size="md"></RoundImage>
@@ -17,7 +17,7 @@ function MessageBubble({ align = 'left', src, alt, nickname, children, time }) {
           <div className="basis-4/5">
             <p className="text-b-2-regular text-bjgray-500">{nickname}</p>
             <div className="flex flex-row items-end gap-2 group-[.group-right]:flex-row-reverse">
-              <div className="flex min-h-9 items-center justify-start whitespace-pre-line break-keep rounded-[10px] rounded-tl-none bg-bjyellow-400 px-4 py-2 text-b-2-medium group-[.group-right]:justify-end group-[.group-left]:rounded-tl-none group-[.group-right]:rounded-tl-[10px] group-[.group-right]:rounded-tr-none">
+              <div className="flex min-h-9 items-center justify-start whitespace-pre-line break-keep rounded-[10px] rounded-tl-none bg-bjgray-200 px-4 py-2 text-b-2-medium group-[.group-right]:justify-end group-[.group-left]:rounded-tl-none group-[.group-right]:rounded-tl-[10px] group-[.group-right]:rounded-tr-none group-[.group-right]:bg-bjyellow-400">
                 {children}
               </div>
               <time
--- src/pages/Chatting/ChatRoom/ChatRoom.jsx
+++ src/pages/Chatting/ChatRoom/ChatRoom.jsx
@@ -1,6 +1,11 @@
 import pb from '@/api/pocketbase';
 import { queryClient } from '@/client/queryClient';
-import { MessageBubble, NomalTitle, ThinTextForm } from '@/components/Atoms';
+import {
+  ChatTextarea,
+  MessageBubble,
+  NomalTitle,
+  ThinTextForm,
+} from '@/components/Atoms';
 import useUserInfoStore from '@/store/useUserInfoStore';
 import { getDocumentTitle, getPbImgs } from '@/utils';
 import { useMutation, useQuery } from '@tanstack/react-query';
@@ -136,7 +141,7 @@ export function ChatRoom() {
       <Helmet>
         <title>{getDocumentTitle(title)}</title>
       </Helmet>
-      <div className="relative flex min-h-svh w-full min-w-[320px] flex-col">
+      <div className="min-h-svh pt-14">
         <NomalTitle
           className="fixed left-[50%] top-0 w-full max-w-[430px] translate-x-[-50%]"
           backLink
@@ -144,41 +149,43 @@ export function ChatRoom() {
         >
           {title}
         </NomalTitle>
-        <main className="flex flex-grow flex-col px-4 py-3">
-          <div className="flex flex-grow flex-col justify-start py-10">
-            <ul
-              ref={chattingListRef}
-              className="mt-1 h-[calc(100vh-56px-40px)] overflow-y-auto bg-slate-50 *:py-[9px]"
-            >
-              {expand.message?.map(
-                ({ id, text, created, expand: { sendUser } }) => (
-                  <MessageBubble
-                    key={id}
-                    align={sendUser.id === userInfo.id ? 'right' : ''}
-                    src={getPbImgs(sendUser)}
-                    alt={sendUser.nickname}
-                    nickname={sendUser.nickname}
-                    time={created.slice(11, 16)}
-                  >
-                    {text}
-                  </MessageBubble>
-                )
-              )}
-            </ul>
-          </div>
-          <div className="mx-4">
-            <div className="fixed bottom-0 left-[50%] mt-auto w-full  max-w-[414px] translate-x-[-50%]">
-              <ThinTextForm
-                className=""
-                onSubmit={handleSendMessage}
-                type="text"
+        <main className="flex h-[calc(100svh-56px)] flex-col">
+          <div className="flex min-h-full flex-col">
+            <div className="bg-bjgray-50 flex flex-1 flex-col overflow-y-auto px-4">
+              <ul ref={chattingListRef} className="mt-auto *:py-[9px]">
+                {expand.message?.map(
+                  ({ id, text, created, expand: { sendUser } }) => (
+                    <MessageBubble
+                      key={id}
+                      align={sendUser.id === userInfo.id ? 'right' : ''}
+                      src={getPbImgs(sendUser)}
+                      alt={sendUser.nickname}
+                      nickname={sendUser.nickname}
+                      time={created.slice(11, 16)}
+                    >
+                      {text}
+                    </MessageBubble>
+                  )
+                )}
+              </ul>
+            </div>
+            <div className="mt-auto px-4 py-3">
+              {/* <ThinTextForm
+                  className=""
+                  onSubmit={handleSendMessage}
+                  type="text"
+                  placeholder="메세지를 입력하세요."
+                  sendButton
+                  id="text"
+                  name="text"
+                >
+                  채팅 메세지
+                </ThinTextForm> */}
+              <ChatTextarea
+                label="메세지 입력창"
+                id="id"
                 placeholder="메세지를 입력하세요."
-                sendButton
-                id="text"
-                name="text"
-              >
-                채팅 메세지
-              </ThinTextForm>
+              />
             </div>
           </div>
         </main>
