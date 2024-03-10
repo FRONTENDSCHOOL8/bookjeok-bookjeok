c12b56966083b4bee36ad74c895c780da05c01d1
jio <pennicillium@daum.net>
2024-03-10 14:36:44 +0900

    [Feat]중복 닉네임 검사 기능 구현 #86

--- src/pages/EditProfile/EditProfile.jsx
+++ src/pages/EditProfile/EditProfile.jsx
@@ -11,33 +11,45 @@ import { Form, useParams } from 'react-router-dom';
 import { useQuery, useMutation } from '@tanstack/react-query';
 import pb from '@/api/pocketbase';
 import useUserInfoStore from '@/store/useUserInfoStore';
+import { useDebounce } from '@/hooks';
+/*
+1. duplicated 닉네임일 경우 하단에 에러메시지
+2. 
 
-const test = {
-  nickname: '하이로로',
-  password: 'qwert123!',
-  passwordConfirm: 'qwert123!',
-  oldPassword: 'qwert123!',
-};
 
+
+*/
 export function EditProfile() {
   const { userId } = useParams();
   const { setUserInfo } = useUserInfoStore((state) => state);
   const [editUserInfo, setEditUserInfo] = useState({});
+  const debouncedData = useDebounce(editUserInfo, 700);
+  console.log('debounced:', debouncedData);
+  const [isDuplicatedNickname, setIsDuplicatedNickname] = useState(false);
+  //중복 닉네임 확인
+  const { data: hasDuplicatedNickname, isSuccess } = useQuery({
+    queryFn: async () => {
+      const fetchedData = await pb
+        .collection('users')
+        .getList(1, 1, { filter: `nickname = "${debouncedData.nickname}"` });
+      return fetchedData.items; // 데이터 반환
+    },
+    queryKey: ['nickname', debouncedData.nickname],
+    //최초실행 방지
+    enabled: !!debouncedData.nickname,
+  });
 
-  // const { data } = useQuery({
-  //   queryFn: async (nickname) => {
-  //     await pb
-  //       .collection('users')
-  //       .getList(1, 1, { filter: `nickname = "${nickname}"` });
-  //   },
-  //   queryKey: ['nickname'],
-  // });
+  console.log(hasDuplicatedNickname?.length);
 
+  // 수정 실행하는 mutation 함수
   const { mutateAsync: updateUsers } = useMutation({
     mutationFn: async () => {
       const userData = await pb
         .collection('users')
         .update(`${userId}`, editUserInfo);
+      return userData;
+    },
+    onSuccess: (userData) => {
       setUserInfo(userData);
     },
   });
@@ -64,9 +76,17 @@ export function EditProfile() {
         <Form className="flex flex-col gap-4 px-4" onChange={handleEditForm}>
           <span>프로필 사진</span>
           <ImageForm id="img" name="img" src={editUserInfo?.img} />
-          <TextForm type="text" id="nickname" name="nickname">
+          <TextForm
+            type="text"
+            id="nickname"
+            name="nickname"
+            description={
+              hasDuplicatedNickname?.length > 0 ? '중복된 닉네임임' : ''
+            }
+          >
             닉네임
           </TextForm>
+
           <TextForm
             type="password"
             id="oldPassword"
