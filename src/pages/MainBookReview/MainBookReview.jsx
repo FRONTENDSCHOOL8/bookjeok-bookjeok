7bc0bcaf31afc6668c8beaae90195324f8d6676b
jio <pennicillium@daum.net>
2024-03-13 10:04:08 +0900

    [Refactor]메인 독후감 debounce, 책 제목 검색으로 변경 #70

--- src/pages/MainBookReview/MainBookReview.jsx
+++ src/pages/MainBookReview/MainBookReview.jsx
@@ -1,17 +1,16 @@
-import { NomalTitle, ThinTextForm } from '@/components/Atoms';
-import { BookReviewList, GNB, MainKindToggle } from '@/components/Molecules';
+import { useDebounce } from '@/hooks';
 import { getDocumentTitle } from '@/utils';
 import { useEffect, useState } from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useLoaderData } from 'react-router-dom';
+import { NomalTitle, ThinTextForm } from '@/components/Atoms';
+import { BookReviewList, GNB, MainKindToggle } from '@/components/Molecules';
 
 export function MainBookReview() {
   const data = useLoaderData();
-  console.log(data);
 
   // 검색창 이벤트 함수
   const handleSearch = (e) => {
-    console.log(e.target.value);
     setSearchKeyword(e.target.value);
     setIsSearchState(e.target.value !== '');
   };
@@ -19,16 +18,15 @@ export function MainBookReview() {
   const [isSearchState, setIsSearchState] = useState(false);
   const [searchKeyword, setSearchKeyword] = useState();
   const [searchResult, setSearchResult] = useState();
-  console.log(searchKeyword);
-  console.log(isSearchState);
+  const debouncedKeyword = useDebounce(searchKeyword, 500);
 
   //검색시 실행되는 이펙트 함수
   useEffect(() => {
     const createValue = data.bookReview.filter((item) =>
-      item['title'].includes(searchKeyword)
+      item['bookTitle'].includes(debouncedKeyword)
     );
     setSearchResult({ resultArray: createValue });
-  }, [data, searchKeyword]);
+  }, [data, debouncedKeyword]);
 
   return (
     <>
@@ -44,7 +42,7 @@ export function MainBookReview() {
               onChange={handleSearch}
               type="search"
               searchIcon
-              placeholder="search"
+              placeholder="책 제목을 입력해주세요"
               className="py-4"
             >
               검색
