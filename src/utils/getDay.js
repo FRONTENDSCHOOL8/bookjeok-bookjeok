3c2cf17055a743f382160f23b3eb3aadb16557bb
clapsheep <sktndid1203@naver.com>
2024-03-13 17:12:34 +0900

    [Fix] 아이폰에서 요일 NaN으로 나오는 문제 해결

--- src/utils/getDay.js
+++ src/utils/getDay.js
@@ -1,6 +1,7 @@
 // yyyy-MM-dd 형식으로 날짜를 받아오면 해당 요일을 계산해주는 함수
 function getDay(yyyyMMdd) {
-  const dayOfWeek = new Date(yyyyMMdd).getDay();
+  const dateStr = yyyyMMdd.replace(/(\d{4})(\d{2})(\d{2})/, '$1-$2-$3');
+  const dayOfWeek = new Date(dateStr).getDay();
   switch (dayOfWeek) {
     case 0:
       return '(일)';
