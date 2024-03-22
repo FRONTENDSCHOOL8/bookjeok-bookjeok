efc8ed3d21bef3d03091a73a7ab1d504cff8e7c3
jio <pennicillium@daum.net>
2024-03-22 17:53:52 +0900

    [Refactor]회원가입 상세정보페이지 리팩토링 #70

--- src/pages/SignUp/DetailInfo.jsx
+++ src/pages/SignUp/DetailInfo.jsx
@@ -1,28 +1,22 @@
-import { useNavigate, Form, useLocation } from 'react-router-dom';
 import {
   MainButton,
   NomalTitle,
   TextForm,
   RadioForm,
 } from '@/components/Atoms';
+import pb from '@/api/pocketbase';
 import { useState, useEffect } from 'react';
 import { useDebounce } from '@/hooks/index';
-import pb from '@/api/pocketbase';
-/*
-1. 만약 signup/detail로 path를 갖게 된다면 basicInfo가 없을시 basicInfo쪽으로 넘겨야됨
-2. 닉네임 중복 확인 , 닉네임 유효성 검사 ? ,,,
-3. 휴대폰 중복 확인 , 휴대폰 유효성검사 .,... 
-  전화번호 입력시 3-4-4 자리수대로 하이픈 .... 
-4. 다음 버튼 클릭시 db 적재  
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useNavigate, Form, useLocation } from 'react-router-dom';
 
-*/
+const INITIAL_STATE = { isDuplicatedNickname: false, isRegisteredPhone: true };
 
 export function DetailInfo() {
   const { state } = useLocation();
   const navigate = useNavigate();
   const [userInfo, setUserInfo] = useState(state);
-  const [isDuplicatedNickname, setIsDuplicatedNickname] = useState(false);
-  const [isRegisteredPhone, setIsRegisteredPhone] = useState(true);
+  const [validateState, setValidateState] = useState(INITIAL_STATE);
   const debouncedUserInfo = useDebounce(userInfo, 500);
 
   const handleUserInfo = (e) => {
@@ -31,38 +25,46 @@ export function DetailInfo() {
   };
 
   // db 적재 !
-  const handleSubmit = () => {
-    pb.collection('users')
-      .create(userInfo)
-      .then(() => {
-        navigate('/welcome');
-      })
-      .catch((Error) => console.error(Error));
-  };
+  const { mutateAsync: handleSubmit } = useMutation({
+    mutationFn: async () => {
+      await pb.collection('users').create(userInfo);
+    },
+    onSuccess: () => {
+      navigate('/welcome');
+    },
+  });
 
-  //닉네임 중복 검사
-  useEffect(() => {
-    if (state && debouncedUserInfo.nickname !== '') {
-      pb.collection('users')
-        .getList(1, 1, {
-          filter: `nickname = "${debouncedUserInfo.nickname}"`,
-        })
-        .then((data) => setIsDuplicatedNickname(data.items.length !== 0))
-        .catch((Error) => console.error(Error));
-    }
-  }, [debouncedUserInfo.nickname]);
+  //닉네임 중복검사
+  useQuery({
+    queryFn: async () => {
+      const data = await pb.collection('users').getList(1, 1, {
+        filter: `nickname = "${debouncedUserInfo.nickname}"`,
+      });
+      setValidateState({
+        ...validateState,
+        ['isDuplicatedNickname']: data.items.length !== 0,
+      });
+      return validateState.isDuplicatedNickname;
+    },
+    queryKey: ['duplicateNickname', debouncedUserInfo.nickname],
+    enabled: state && debouncedUserInfo.nickname !== '',
+  });
 
-  // 가입된 휴대전화 검사
-  useEffect(() => {
-    if (state && debouncedUserInfo.phone !== '') {
-      pb.collection('users')
-        .getList(1, 1, {
-          filter: `phone = "${debouncedUserInfo.phone}"`,
-        })
-        .then((data) => setIsRegisteredPhone(data.items.length !== 0))
-        .catch((Error) => console.error(Error));
-    }
-  }, [debouncedUserInfo.phone]);
+  //전화번호 중복검사
+  useQuery({
+    queryFn: async () => {
+      const data = await pb.collection('users').getList(1, 1, {
+        filter: `phone = "${debouncedUserInfo.phone}"`,
+      });
+      setValidateState({
+        ...validateState,
+        ['isRegisteredPhone']: data.items.length !== 0,
+      });
+      return validateState.isRegisteredPhone;
+    },
+    queryKey: ['isRegisteredPhoneNumber', debouncedUserInfo.phone],
+    enabled: state && debouncedUserInfo.phone !== '',
+  });
 
   //기본 정보 미기입 후 주소창 /signup/detail 페이지로 이동시 강제로 기본정보 입력 페이지로 이동
   useEffect(() => {
@@ -87,7 +89,7 @@ export function DetailInfo() {
               maxLength={10}
               onChange={handleUserInfo}
               description={
-                userInfo.nickname && isDuplicatedNickname
+                userInfo.nickname && validateState.isDuplicatedNickname
                   ? '이미 사용 중인 닉네임 입니다. '
                   : ''
               }
@@ -101,7 +103,7 @@ export function DetailInfo() {
               maxLength={11}
               onChange={handleUserInfo}
               description={
-                userInfo.phone && isRegisteredPhone
+                userInfo.phone && validateState.isRegisteredPhone
                   ? '이미 가입된 전화번호입니다!'
                   : ''
               }
@@ -153,13 +155,13 @@ export function DetailInfo() {
             as="button"
             disabled={
               !(
-                !isRegisteredPhone &&
+                !validateState.isRegisteredPhone &&
                 userInfo.birth !== '' &&
                 userInfo.gender !== '' &&
-                !isDuplicatedNickname
+                !validateState.isDuplicatedNickname
               )
             }
-            onClick={handleSubmit}
+            onClick={async () => await handleSubmit()}
           >
             다음
           </MainButton>
