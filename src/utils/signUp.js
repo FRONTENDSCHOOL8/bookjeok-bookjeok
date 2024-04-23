b454930617c09697cf40dac37724fc94fd9869e2
jio <pennicillium@daum.net>
2024-04-23 20:38:07 +0900

    [Modify] 회원가입시 사용하는 에러메시지 유틸함수 수정 #70

--- src/utils/signUp.js
+++ src/utils/signUp.js
@@ -1,5 +1,5 @@
 export const getDescriptionEmail = (email, state) => {
-  if (email == '') return;
+  if (!email) return;
   if (!state.isValidateEmail) {
     return '이메일 형식이 올바르지 않습니다.';
   }
@@ -8,14 +8,14 @@ export const getDescriptionEmail = (email, state) => {
   }
 };
 export const getDescriptionPassword = (password, state) => {
-  if (password == '') return;
+  if (!password) return;
   if (!state.isValidatePassword) {
     return '비밀번호는 8자 이상 영문, 숫자, 특수문자를 포함해 작성해주세요';
   }
 };
 
 export const getDescriptionConfirmPassword = (confirmPassword, state) => {
-  if (confirmPassword == '' || confirmPassword == undefined) return;
+  if (!confirmPassword || confirmPassword == undefined) return;
   if (!state.isConfirmPassword) {
     return '동일한 비밀번호를 입력해주세요.';
   }
