76a734cd1e58a62c1578bad25a87e5d550aafb95
jio <pennicillium@daum.net>
2024-04-26 22:10:30 +0900

    [Chore] fetchReadData 삭제 #70

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,7 +1,6 @@
 export { default as getCreatedHoursAgo } from '@/utils/getCreatedHoursAgo';
 export { default as createNumberArray } from '@/utils/createNumberArray';
 export { default as getDocumentTitle } from '@/utils/getDocumentTitle.ts';
-export { default as fetchReadDataAPI } from '@/utils/fetchReadDataAPI';
 export { default as validatePassword } from '@/utils/validatePassword.ts';
 export { default as createRandomId } from '@/utils/createRandomId';
 export { default as validateEmail } from '@/utils/validateEmail.ts';
