64f7b3fea33561f51bcd1a80bcac6b42b82b8d76
clapsheep <sktndid1203@naver.com>
2024-05-17 23:43:34 +0900

    [Feat] intro페이지 test 계정 추가

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -21,7 +21,8 @@ export function Intro() {
       const tl = gsap.timeline();
       tl.from('.intro-animation', { opacity: '0', duration: 1, y: 50 })
         .from('.view-around', { opacity: '0', duration: 0.5, y: -30 }, '-=0.5')
-        .from('.login-animation', { opacity: '0', duration: 0.5, y: 30 }, '<');
+        .from('.login-animation', { opacity: '0', duration: 0.5, y: 30 }, '<')
+        .from('.test', { opacity: '0', duration: 0.5 }, '<');
     },
     { scope: container }
   );
@@ -38,16 +39,21 @@ export function Intro() {
           <Svg size={60} id="logo" />
           <h1 className="text-h-1-semibold">북적북적</h1>
         </div>
-        <div className="absolute bottom-0 mx-auto flex h-[20%] w-56 flex-col items-center gap-4">
+        <div className="absolute bottom-0 mx-auto flex h-[40%] w-56 flex-col items-center gap-8">
+          <div className="test flex flex-col justify-center rounded-4xl bg-bjyellow-100 px-12 py-4 text-center text-b-0-medium opacity-40">
+            <span>테스트 계정</span>
+            <span>ID : wat@ch.me</span>
+            <span>PW : qwert123!</span>
+          </div>
           <MainButton
             size="sm"
-            className="view-around shadow-xl"
+            className="view-around text-b-0-medium shadow-xl"
             to="/main/club"
             color="third"
           >
             둘러보기
           </MainButton>
-          <div className="login-animation flex w-56 items-center gap-2">
+          <div className="login-animation flex w-56 items-center gap-2 text-b-0-medium">
             <MainButton
               size="sm"
               className="p-4 shadow-xl"
@@ -58,7 +64,7 @@ export function Intro() {
             </MainButton>
             <MainButton
               size="sm"
-              className="p-4 shadow-xl"
+              className="p-4 text-b-0-medium shadow-xl"
               color="third"
               to="/signup"
             >
