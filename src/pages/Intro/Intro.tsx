63edf0576d929106f7874151d0eb69487ba3f3ef
clapsheep <sktndid1203@naver.com>
2024-05-17 23:50:26 +0900

    [Style] intro 아이콘 위치조정

--- src/pages/Intro/Intro.tsx
+++ src/pages/Intro/Intro.tsx
@@ -35,11 +35,11 @@ export function Intro() {
         ref={container}
         className="flex min-h-svh w-full justify-center bg-bjyellow-400"
       >
-        <div className="intro-animation absolute top-[30%] flex flex-col items-center gap-2">
+        <div className="intro-animation absolute top-[25%] flex flex-col items-center gap-2">
           <Svg size={60} id="logo" />
           <h1 className="text-h-1-semibold">북적북적</h1>
         </div>
-        <div className="absolute bottom-0 mx-auto flex h-[40%] w-56 flex-col items-center gap-8">
+        <div className="absolute bottom-0 mx-auto flex h-[45%] w-56 flex-col items-center gap-4">
           <div className="test flex flex-col justify-center rounded-4xl bg-bjyellow-100 px-12 py-4 text-center text-b-0-medium opacity-40">
             <span>테스트 계정</span>
             <span>ID : wat@ch.me</span>
