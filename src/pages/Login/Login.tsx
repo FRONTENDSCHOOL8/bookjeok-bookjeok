60bb00489d968e911875aefda9fe6f6754a7e80e
jio <pennicillium@daum.net>
2024-05-29 10:14:54 +0900

    [Refactor] 로그인 로딩 애니메이션 추가 #70

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,14 @@
 import pb from '@/api/pocketbase';
 import { useCloseModal } from '@/hooks';
 import { Helmet } from 'react-helmet-async';
+import { Loading } from '@/components/Common';
+import { useNavigate } from 'react-router-dom';
 import { useQuery } from '@tanstack/react-query';
-import { useRef, useState, useCallback, useEffect } from 'react';
+import { useRef, useState, useCallback } from 'react';
 import useUserInfoStore from '@/store/useUserInfoStore';
 import { getDocumentTitle, validateEmail } from '@/utils';
 import { DobbleButtonModal } from '@/components/Molecules';
 import { TextForm, NomalTitle, MainButton } from '@/components/Atoms';
-import { useNavigate } from 'react-router-dom';
 
 export function Login() {
   const emailRef = useRef('');
@@ -31,7 +32,7 @@ export function Login() {
     setIsClicked(true);
   }, []);
 
-  const { isSuccess } = useQuery({
+  const { isSuccess, isPending } = useQuery({
     queryKey: ['login'],
     queryFn: async () => {
       try {
@@ -63,50 +64,54 @@ export function Login() {
 
   return (
     <>
-      <div className="flex min-h-svh flex-col">
-        <Helmet>
-          <title>{getDocumentTitle('로그인')}</title>
-        </Helmet>
-        <NomalTitle backLink>로그인</NomalTitle>
-        <form
-          onSubmit={handleLogin}
-          className="flex flex-grow flex-col gap-y-4 p-4"
-          onChange={handleLoginForm}
-        >
-          <TextForm
-            type="email"
-            id="email"
-            placeholder="email@email.com"
-            name="email"
+      {isClicked && isPending ? (
+        <Loading />
+      ) : (
+        <div className="flex min-h-svh flex-col">
+          <Helmet>
+            <title>{getDocumentTitle('로그인')}</title>
+          </Helmet>
+          <NomalTitle backLink>로그인</NomalTitle>
+          <form
+            onSubmit={handleLogin}
+            className="flex flex-grow flex-col gap-y-4 p-4"
+            onChange={handleLoginForm}
           >
-            이메일
-          </TextForm>
-          <TextForm
-            type="password"
-            id="password"
-            placeholder=""
-            name="password"
+            <TextForm
+              type="email"
+              id="email"
+              placeholder="email@email.com"
+              name="email"
+            >
+              이메일
+            </TextForm>
+            <TextForm
+              type="password"
+              id="password"
+              placeholder=""
+              name="password"
+            >
+              비밀번호
+            </TextForm>
+            <div className="mt-auto">
+              <MainButton className="mt-auto p-4" as="button" type="submit">
+                로그인
+              </MainButton>
+            </div>
+          </form>
+          <DobbleButtonModal
+            open={isModalOpen}
+            closeButton
+            svgId="logo"
+            title={isSuccess ? '로그인 성공' : '로그인 실패'}
+            onClick={() => setIsModalOpen(false)}
+            primaryButtonText={isSuccess ? '홈으로' : undefined}
+            primaryButtonPath={isSuccess ? '/Main/club' : undefined}
           >
-            비밀번호
-          </TextForm>
-          <div className="mt-auto">
-            <MainButton className="mt-auto p-4" as="button" type="submit">
-              로그인
-            </MainButton>
-          </div>
-        </form>
-        <DobbleButtonModal
-          open={isModalOpen}
-          closeButton
-          svgId="logo"
-          title={isSuccess ? '로그인 성공' : '로그인 실패'}
-          onClick={() => setIsModalOpen(false)}
-          primaryButtonText={isSuccess ? '홈으로' : undefined}
-          primaryButtonPath={isSuccess ? '/Main/club' : undefined}
-        >
-          {isSuccess ? null : '계정 정보를 확인해주세요'}
-        </DobbleButtonModal>
-      </div>
+            {isSuccess ? null : '계정 정보를 확인해주세요'}
+          </DobbleButtonModal>
+        </div>
+      )}
     </>
   );
 }
