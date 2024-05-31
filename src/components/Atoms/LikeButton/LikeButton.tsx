35fe1390d859c00862cbff68dc71bc17262ed319
yuna <rnssue@naver.com>
2024-05-31 10:17:59 +0900

    [Style] 좋아요 버튼 수정

--- src/components/Atoms/LikeButton/LikeButton.tsx
+++ src/components/Atoms/LikeButton/LikeButton.tsx
@@ -18,7 +18,7 @@ const LikeButton = ({
     <button
       type="button"
       onClick={onClick}
-      className={`absolute right-3 top-[150px] ${className}`}
+      className={`absolute bottom-3 right-3 ${className}`}
       aria-label="좋아요"
     >
       {active ? (
