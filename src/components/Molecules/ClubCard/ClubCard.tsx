beb2800d104e33138773223effc7c7e4151161c5
yuna <rnssue@naver.com>
2024-05-31 10:14:49 +0900

    [Style] 좋아요 버튼 수정

--- src/components/Molecules/ClubCard/ClubCard.tsx
+++ src/components/Molecules/ClubCard/ClubCard.tsx
@@ -108,46 +108,48 @@ const ClubCard = ({
           <Badge className="absolute left-2 top-2 w-[30%]">
             {expand?.genre.title}
           </Badge>
-          <div className="flex w-full flex-col gap-y-1 px-1 py-4 pt-3">
-            <div className="flex justify-between">
-              <h3 className="line-clamp-2 h-12 max-w-full text-b-1-regular">
-                {title}
-              </h3>
-            </div>
-            <span className="text-pretty text-b-3-medium text-bjgray-500">
-              <Svg
-                color="#9e9e9e"
-                size={14}
-                id="calendar"
-                className="mr-[2px] inline-block align-middle"
-              />
-              <span className="align-middle">{convertTime(dateTime, 1)}</span>
-            </span>
-            <div className="flex justify-between">
-              <span className="flex items-center text-pretty text-b-3-medium text-bjgray-500">
-                <Svg
-                  color="#9e9e9e"
-                  size={14}
-                  id="pin"
-                  className="mr-[2px] flex-shrink-0 align-middle"
-                />
-                <span className="line-clamp-1">
-                  {!isOffline ? '온라인' : location}
-                </span>
-              </span>
-              <span className="flex items-center text-b-3-medium text-bjgray-500">
-                <Svg color="#9e9e9e" size={14} id="user" className="mr-[2px]" />
-                {confirmUser.length ? confirmUser.length : 0}/{limitPerson}
-              </span>
-            </div>
-          </div>
-        </Link>
+        </Link>{' '}
         <LikeButton
           onClick={userInfo ? handleLike : undefined}
           id={id}
           active={userInfo ? likeState?.includes(userInfo!.id) : false}
         />
       </figure>
+      <Link to={`/club/${id}`} aria-label={`${title}`}>
+        <div className="flex w-full flex-col gap-y-1 px-1 py-4 pt-3">
+          <div className="flex justify-between">
+            <h3 className="line-clamp-2 h-12 max-w-full text-b-1-regular">
+              {title}
+            </h3>
+          </div>
+          <span className="text-pretty text-b-3-medium text-bjgray-500">
+            <Svg
+              color="#9e9e9e"
+              size={14}
+              id="calendar"
+              className="mr-[2px] inline-block align-middle"
+            />
+            <span className="align-middle">{convertTime(dateTime, 1)}</span>
+          </span>
+          <div className="flex justify-between">
+            <span className="flex items-center text-pretty text-b-3-medium text-bjgray-500">
+              <Svg
+                color="#9e9e9e"
+                size={14}
+                id="pin"
+                className="mr-[2px] flex-shrink-0 align-middle"
+              />
+              <span className="line-clamp-1">
+                {!isOffline ? '온라인' : location}
+              </span>
+            </span>
+            <span className="flex items-center text-b-3-medium text-bjgray-500">
+              <Svg color="#9e9e9e" size={14} id="user" className="mr-[2px]" />
+              {confirmUser.length ? confirmUser.length : 0}/{limitPerson}
+            </span>
+          </div>
+        </div>
+      </Link>
     </li>
   );
 };
